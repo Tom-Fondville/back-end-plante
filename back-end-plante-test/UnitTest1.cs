9674aea15bcce96e308bb3a053932066c9e0715f
tom-fondville <tomfondville@gmail.com>
2023-06-30 15:03:13 +0200

    test projetc

---
+++ back-end-plante-test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace back_end_plante_test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
