89dfc609aaef68296009842c2ee8d7ac8e4714f9
Gaston Pallas <pallasgaston@gmail.com>
2023-06-30 15:15:15 +0200

    test unitaire

---
+++ back-end-plante-test/Utils/HasherTest.cs
@@ -0,0 +1,24 @@
+﻿using back_end_plante.Utils;
+
+namespace back_end_plante_test.Utils;
+
+
+public class HasherTest
+{
+
+    [Test]
+    public void test_hasher()
+    {
+        //Variables
+        String password = "password";
+
+        // Class to test
+        String result = Hasher.Hash(password);
+
+        //Asserts
+        Assert.AreNotEqual(password, result);
+
+    }
+    
+    
+}
